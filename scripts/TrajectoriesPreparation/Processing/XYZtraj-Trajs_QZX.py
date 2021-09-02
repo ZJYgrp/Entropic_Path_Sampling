4ea7e9c83dbb5600bb5e52b464fb4f48b928bf1a
Laura-zj <64767132+Laura-zj@users.noreply.github.com>
2021-09-02 22:29:12 +0800

    README
    Update readme.md based on the adjustment of workflow and make some supplements to some related operations.

--- scripts/TrajectoriesPreparation/Processing/XYZtraj-Trajs_QZX.py
+++ scripts/TrajectoriesPreparation/Processing/XYZtraj-Trajs_QZX.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # This script should be revised to an object-oriented program, which defines TS extraction class, trajectories classfication class, etc.
 import numpy as np
 import sys
